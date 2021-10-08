66327e9ca093363ecada9d43662bf713929c2aae
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-10-08 13:46:34 +0300

    Import data from file

---
+++ Persistance/FileExtractContent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistance
+{
+    class FileExtractContent
+    { public string Path { get; set; }
+        public FileExtractContent(string Path)
+        {
+            this.Path = Path;
+        }
+        public List<List<string>> ImportData()
+        {
+            List<List<string>> data=new();
+            string[] lines = System.IO.File.ReadAllLines(Path);
+            for(int i=1;i<lines.Length;i++)
+            {
+                char[] separators = new char[] { ' ',  '*' };
+                data.Add(new List<string>(lines[i].Split(separators, StringSplitOptions.RemoveEmptyEntries)));
+
+            }
+            return data;
+
+
+        }
+
+    }
+}
+
+
---
+++ Persistance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistance
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> date = new();
+            FileExtractContent file1 = new("C:\\Users\\prico\\Downloads\\weather.dat");
+            date = file1.ImportData();
+            foreach (List<string> line in date)
+            {
+                foreach (string word in line)
+                    Console.Write(word + " ");
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
